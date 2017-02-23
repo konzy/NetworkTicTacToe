11fbf2764cdb36036afb03c7b85b688089284b30
Brian <bkonzman@gmail.com>
2017-02-23 00:13:14 -0500

    correct win condition checking

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,13 +6,15 @@ import java.util.Random;
 /**
  * Created by konzy on 2/21/2017.
  */
-public class Game {
-
-    private String[][] board = new String[3][3];
-    private Player currentPlayer;
-    private Random random;
+public class Game implements Cloneable {
 
     private static final String BLANK = " ";
+    private static final String X_LETTER = "X";
+    private static final String O_LETTER = "O";
+    private String[][] board = new String[3][3];
+    private Player currentPlayer;
+    private String currentSymbol = X_LETTER;
+    private Random random;
 
     enum Player {
         HUMAN,
@@ -28,14 +30,42 @@ public class Game {
     public Game(Player currentPlayer) {
         random = new Random();
         initializeBoard();
+        getRandomCurrentPlayer();
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            Game clone = (Game)super.clone();
+            clone.board = board.clone();
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public boolean playMove(int x, int y) {
         if (board[x][y].equals(BLANK)) {
-            board[x][y] = "X";
+            board[x][y] = currentSymbol;
+            switchCurrentPlayer();
             return true;
         }
         return false;
+    }
+
+    private void switchCurrentPlayer() {
+        if (currentPlayer == Player.HUMAN) {
+            currentPlayer = Player.CPU;
+        } else {
+            currentPlayer = Player.HUMAN;
+        }
+
+        if (currentSymbol.equals(X_LETTER)) {
+            currentSymbol = O_LETTER;
+        } else {
+            currentSymbol = X_LETTER;
+        }
     }
 
     private void getRandomCurrentPlayer() {
@@ -50,25 +80,32 @@ public class Game {
     private void initializeBoard() {
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
-                board[x][y] = " ";
+                board[x][y] = BLANK;
             }
         }
     }
 
     private GameState getGamestate() {
-        if ((board[0] == board[1] && board[1] == board[2]) ||
-                (board[3] == board[4] && board[4] == board[5]) ||
-                (board[6] == board[7] && board[7] == board[8]) ||
-                (board[0] == board[3] && board[3] == board[6]) ||
-                (board[1] == board[4] && board[4] == board[7]) ||
-                (board[2] == board[5] && board[5] == board[8]) ||
-                (board[0] == board[4] && board[4] == board[8]) ||
-                (board[2] == board[4] && board[4] == board[6])) {
+        if (isWin()) {
             return GameState.WIN;
         } else if (isBoardFull()) {
             return GameState.DRAW;
         }
         return GameState.PLAY;
+    }
+
+    private boolean isWin() {
+        if ((board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2]) && !board[0][0].equals(BLANK)) ||
+                (board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0]) && !board[0][2].equals(BLANK))) {
+            return true;
+        }
+        for (int i = 0; i < 3; i++) {
+            if ((board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i]) && !board[0][i].equals(BLANK)) ||
+                    (board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2]) && !board[i][0].equals(BLANK))){
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean isBoardFull () {
@@ -81,6 +118,4 @@ public class Game {
         }
         return true;
     }
-
-
 }
